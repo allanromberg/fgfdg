dd5e90ee33f62b6639fd73928bd823c46b9ad603
Anna Sandin <Anna.Sandin@hiq.se>
2020-10-22 14:16:22 +0200

    calculatorv1

---
+++ miniräknare/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace miniräknare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("mata in tal 1:");
+            int inmatat = int.Parse(Console.ReadLine());
+            Console.WriteLine(inmatat);
+            Console.Write("mata in tal 2: ");
+            int inmatat2  = int.Parse (Console.ReadLine());
+            int summa = inmatat + inmatat2;
+            Console.WriteLine("summan: "+ summa);
+            int produkt = inmatat * inmatat2;
+            Console.WriteLine("produkt: "+ produkt);
+            int kvot = inmatat / inmatat2;
+            Console.WriteLine("kvot: " + kvot);
+            Console.ReadLine();
+        }
+    }
+}
